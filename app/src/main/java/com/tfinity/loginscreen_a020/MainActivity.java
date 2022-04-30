5dd55885511e3493f829cc0d07664c1f05f3882f
Talha Riaz <publishappstore74@gmail.com>
2022-04-30 20:41:14 +0500

    Final Result

--- app/src/main/java/com/tfinity/loginscreen_a020/MainActivity.java
+++ app/src/main/java/com/tfinity/loginscreen_a020/MainActivity.java
@@ -2,13 +2,25 @@ package com.tfinity.loginscreen_a020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView signup;
+
+    public void onClickSignup(View v){
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup=findViewById(R.id.signupLogin);
+
     }
 }
--- app/src/main/java/com/tfinity/loginscreen_a020/Signup.java
+++ app/src/main/java/com/tfinity/loginscreen_a020/Signup.java
@@ -2,13 +2,24 @@ package com.tfinity.loginscreen_a020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
+
+    TextView login;
+
+    public void onClickLogin(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        login=findViewById(R.id.LoginSignup);
     }
 }
